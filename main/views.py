ce3658c21cc08a381b01b74a2b82b8efc111a2f5
summerlunaa <rhgus1085@gmail.com>
2021-11-22 15:21:48 +0900

    Improve readability - banckend code

--- main/views.py
+++ main/views.py
@@ -1,32 +1,33 @@
 # main/views.py
-from rest_framework import viewsets, generics
+from rest_framework import generics, viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Post, DoneRegister, Comment
-from .serializers import PostSerializer, DoneRegisterSerializer, CommentSerializer
-from .permissions import IsAuthorOrReadOnly
-from django.views.decorators.csrf import ensure_csrf_cookie
 
+from .models import Comment, DoneRegister, Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (CommentSerializer, DoneRegisterSerializer,
+                          PostSerializer)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly,
-                        IsAuthorOrReadOnly]
+                          IsAuthorOrReadOnly]
 
     # post를 작성하면 serializer를 저장함
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-    
+
     # online 카테고리의 글들을 보여주는 view
     @action(detail=False)
     def online(self, request, pk=None):
         serializer_context = {'request': request}
         queryset = Post.objects.filter(category='Online')
-        serializer = PostSerializer(queryset, many=True, context=serializer_context)
+        serializer = PostSerializer(
+            queryset, many=True, context=serializer_context)
         return Response(serializer.data)
 
     # offline 카테고리의 글들을 보여주는 view
@@ -34,7 +35,8 @@ class PostViewSet(viewsets.ModelViewSet):
     def offline(self, request, pk=None):
         serializer_context = {'request': request}
         queryset = Post.objects.filter(category='Offline')
-        serializer = PostSerializer(queryset, many=True, context=serializer_context)
+        serializer = PostSerializer(
+            queryset, many=True, context=serializer_context)
         return Response(serializer.data)
 
     # delivery 카테고리의 글들을 보여주는 view
@@ -42,7 +44,8 @@ class PostViewSet(viewsets.ModelViewSet):
     def delivery(self, request, pk=None):
         serializer_context = {'request': request}
         queryset = Post.objects.filter(category='Delivery')
-        serializer = PostSerializer(queryset, many=True, context=serializer_context)
+        serializer = PostSerializer(
+            queryset, many=True, context=serializer_context)
         return Response(serializer.data)
 
 
@@ -60,8 +63,8 @@ class DoneRegisterView(generics.ListCreateAPIView):
 class DoneView(APIView):
 
     def post(self, request, id, format=None):
-        post = Post.objects.get(id = id)
-        done_register = DoneRegister.objects.get(post_id = id)
+        post = Post.objects.get(id=id)
+        done_register = DoneRegister.objects.get(post_id=id)
 
         # 유저가 참여한 공동구매, 즉 post.members에 유저가 있는 경우
         if post.members.filter(nickname=self.request.user.nickname):
@@ -85,7 +88,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly,
-                        IsAuthorOrReadOnly]
+                          IsAuthorOrReadOnly]
 
     # post를 작성하면 serializer를 저장함
     def perform_create(self, serializer):
@@ -102,19 +105,23 @@ class CommentListView(generics.ListAPIView):
 
 class PostJoinView(APIView):
 
-    # @ensure_csrf_cookie
     def post(self, request, id, format=None):
-        post = Post.objects.get(id = id)
+        post = Post.objects.get(id=id)
+        # 유저가 이미 공동구매 참여중, 즉 post.members에 유저가 있는 경우
         if post.members.filter(nickname=self.request.user.nickname):
+            # 유저가 글 작성자인 경우
             if self.request.user == post.author:
                 return Response("작성자는 참여를 취소할 수 없습니다.")
+            # 유저가 공동구매 참여를 취소하는 경우
             else:
                 post.members.remove(self.request.user)
                 post.save()
-                return Response("Unparticipate success")
+                return Response("공동구매 참여 취소를 완료하였습니다.")
+        # 아직 공동구매 참여자 수가 limit보다 작은 경우
         elif post.members.count() < post.limit:
             post.members.add(self.request.user)
             post.save()
-            return Response("Participate success")
+            return Response("공동구매 참여를 완료하였습니다.")
+        # 공동구매 참여자 수가 limit을 달성한 경우
         else:
             return Response("인원이 모두 찼습니다.")
