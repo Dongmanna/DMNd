a6ae81616da732d543e954e28b474d5ac8972740
summerlunaa <rhgus1085@gmail.com>
2021-11-23 13:21:28 +0900

    for heroku

--- dmnproject/settings.py
+++ dmnproject/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'dmn-front/build/static')
+    os.path.join(BASE_DIR, 'build/static')
 ]
 
 # 이미지를 위한 설정
